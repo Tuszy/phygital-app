7ad827a323d0f69e52dc4157d09038ac612defe2
Dennis T <denniro@gmail.com>
2023-11-16 14:15:52 +0100

    add id getter to Phygital

--- lib/model/phygital.dart
+++ lib/model/phygital.dart
@@ -1,11 +1,19 @@
+import 'dart:typed_data';
+
+import 'package:ndef/utilities.dart';
+import 'package:pointycastle/api.dart';
+import 'package:web3dart/credentials.dart';
+
 class Phygital {
   Phygital({required this.address, required this.contractAddress});
 
-  final String address;
-  final String? contractAddress;
+  final EthereumAddress address;
+  EthereumAddress? contractAddress;
+
+  Uint8List get id => Digest('Keccak/256').process(address.hexNo0x.padLeft(64, '0').toBytes());
 
   @override
   String toString() {
-    return "Phygital Address: $address\n\n${contractAddress != null ? "Contract Address: $contractAddress\n" : ""}";
+    return "Phygital Address: ${address.hexEip55}\n\n${contractAddress != null ? "Contract Address: ${contractAddress!.hexEip55}\n" : ""}";
   }
 }
