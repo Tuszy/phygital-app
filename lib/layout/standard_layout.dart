743b5e82e0898db713a8407d8d20c01fc1619f31
Dennis T <denniro@gmail.com>
2023-11-18 20:14:17 +0100

    add optional confirm button to standard layout

--- lib/layout/standard_layout.dart
+++ lib/layout/standard_layout.dart
@@ -6,10 +6,11 @@ import 'package:flutter/material.dart';
 import '../component/loading_backdrop.dart';
 
 class StandardLayout extends StatefulWidget {
-  const StandardLayout({super.key, required this.child, this.title});
+  const StandardLayout({super.key, required this.child, this.title, this.onConfirm});
 
   final String? title;
   final Widget child;
+  final VoidCallback? onConfirm;
 
   @override
   State<StandardLayout> createState() => _StandardLayoutState();
@@ -69,22 +70,78 @@ class _StandardLayoutState extends State<StandardLayout>
                   vsync: this,
                   child: const ColoredBox(color: Colors.transparent)),
             ),
-            LayoutBuilder(
-              builder:
-                  (BuildContext context, BoxConstraints viewportConstraints) {
-                return SingleChildScrollView(
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      minHeight: viewportConstraints.maxHeight,
+            Column(
+              children: [
+                Expanded(
+                  child: LayoutBuilder(
+                    builder: (BuildContext context,
+                        BoxConstraints viewportConstraints) {
+                      return SingleChildScrollView(
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(
+                            minHeight: viewportConstraints.maxHeight,
+                          ),
+                          child: IntrinsicHeight(
+                            child: SafeArea(
+                              child: widget.child,
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                if(widget.onConfirm != null)
+                Container(
+                  decoration: const BoxDecoration(
+                    //color: Color(0x8800ffff),
+                    border: Border(
+                      top: BorderSide(width: 2, color: Colors.white38),
                     ),
-                    child: IntrinsicHeight(
-                      child: SafeArea(
-                        child: widget.child,
+                    color: Color(0x7700ff00),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(0x7700ff00),
+                        spreadRadius: 5,
+                        blurRadius: 8,
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(
+                        sigmaX: 3,
+                        sigmaY: 3,
+                      ),
+
+                      /// Filter effect field
+                      child: TextButton(
+                        style: TextButton.styleFrom(
+                          foregroundColor: const Color(0xcdffffff),
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 16, vertical: 24),
+                          textStyle: const TextStyle(
+                            letterSpacing: 3,
+                            fontSize: 24,
+                            fontWeight: FontWeight.w900,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(4),
+                          ),
+                        ),
+                        onPressed: () {
+                          widget.onConfirm!();
+                        },
+                        child: const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [Text("CONFIRM")],
+                        ),
                       ),
                     ),
                   ),
-                );
-              },
+                ),
+              ],
             ),
             const LoadingBackdrop()
           ],
