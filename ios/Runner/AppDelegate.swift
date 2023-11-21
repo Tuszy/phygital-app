92b1a12debc7de0fb5b49c26a8acb1fce9e04d07
Dennis T <denniro@gmail.com>
2023-11-21 13:43:45 +0100

    add appcenter for ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,4 +1,8 @@
 import UIKit
+import AppCenter
+import AppCenterAnalytics
+import AppCenterCrashes
+import AppCenterDistribute
 import Flutter
 
 @UIApplicationMain
@@ -8,6 +12,21 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+      
+#if DEBUG
+            AppCenter.start(withAppSecret: "4f3ac864-2039-446c-9b66-33767dfaa0a1", services:[
+              Analytics.self,
+              Crashes.self
+            ])
+    #else
+            AppCenter.start(withAppSecret: "4f3ac864-2039-446c-9b66-33767dfaa0a1", services:[
+              Analytics.self,
+              Crashes.self,
+              Distribute.self
+            ])
+      #endif
+      
+      
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
